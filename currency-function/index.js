01e47623c0c37886aaa0c698f20fb13a72511079
malupressler <malupressler@gmail.com>
2024-03-18 21:52:33 -0300

    fix: the code was not working as expected

--- currency-function/index.js
+++ currency-function/index.js
@@ -1,19 +1,30 @@
 function formatCurrencyInput(value) {
-    if (typeof value !== 'string' || value.match(/\D/)) {
-        console.error('The parameter value is not a valid currency input');
-        return '';
+    if (typeof value !== 'string' || value.match(/[^\d.,]/)) {
+        console.error('The parameter value is not a valid, since is not a string');
+        return ''
+    }
+    value = value.replace(/\D/g, '');
+
+    if (value.length >= 5 && value.startsWith("00")) {
+        value = value.slice(2);
     }
 
-    const cleanValue = value.replace(/\D/g, '');
+    if (value.length > 2) {
+        value = value.slice(0, -2) + ',' + value.slice(-2); 
+        if (value.startsWith("0")){
+            value = value.slice(1);
+        }
+    } 
 
-    if (cleanValue.length === 0) {
-        return '0,00';
+    else if (value.length === 1){
+        value = '0,0' + value;
+    } 
+
+    else {
+        value = '0,' + value;
     }
 
-    let integerPart = cleanValue.slice(0, -2).replace(/^0+/, ''); // Remove leading zeros
-    let decimalPart = cleanValue.slice(-2).padStart(2, '0');
-
-    return `${integerPart || '0'},${decimalPart}`;
+    return value;
 }
 
 export default formatCurrencyInput;
